63e06edfa9b6f9eff148466dcbb7fc10fca63d9e
Xing <Xing@qq.com>
2021-07-06 12:31:01 +0800

    second commit

--- src/main/java/com/xxz/git/GitTest.java
+++ src/main/java/com/xxz/git/GitTest.java
@@ -7,5 +7,6 @@ package com.xxz.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello world!");
     }
 }
