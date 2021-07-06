74ba0c1b1dbff983f2790e2386d0b58f3f734d8a
Xing <Xing@qq.com>
2021-07-06 19:29:01 +0800

    master commit2

--- src/main/java/com/xxz/git/GitTest.java
+++ src/main/java/com/xxz/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello!");
         System.out.println("hello111!");
         System.out.println("hello222!");
+        System.out.println("hello333!");
     }
 }
